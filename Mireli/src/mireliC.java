3bf47c622839a97d5056cbba931e635877fc3d05
mireliloya <mireliloya@192.168.0.19>
2019-05-29 14:33:42 -0700

    segunda clase

--- Mireli/src/mireliC.java
+++ Mireli/src/mireliC.java
@@ -4,6 +4,8 @@ public class mireliC {
 	public static void main(String[] args) {
 System.out.println("Hello World");
 System.out.println("estoy loca");
+new segunDaClase();
+
 	}
 
 }
---
+++ Mireli/src/segunDaClase.java
@@ -0,0 +1,9 @@
+
+public class segunDaClase {
+int g = 6 ;
+
+public segunDaClase() {
+
+System.out.println("what is int g "+g );
+}
+}
